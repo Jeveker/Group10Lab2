268a6355904696040dfe4f98e12aa1c893cb4901
ginah903 <ginah903@gmail.com>
2025-02-27 17:48:38 -0600

    Pushing finished MiddleEarthCharacter class, completed attack, getRace, and displayInfo() methods

--- src/middleearthmadness/character/MiddleEarthCharacter.java
+++ src/middleearthmadness/character/MiddleEarthCharacter.java
@@ -18,9 +18,28 @@ public abstract class MiddleEarthCharacter {
 		this.power = power;
 	}
 
+	/**
+	 * Abstract method that returns true if attack is successful and reduced target's
+	 * health, returns false if attack was ineffective or against their own kin.
+	 * @param target The character to attack
+	 */
+	public boolean attack(MiddleEarthCharacter target);
 	
-
+	/**
+	 * Abstract method to return the race of the character.
+	 */
+	public String getRace();
 	
+	/**
+	 * Prints character details.
+	 */
+	public void displayInfo() {
+		System.out.println("Character details...");
+		System.out.println("Name: " + name);
+		System.out.println("Health: " + health);
+		System.out.println("Power: " + name);
+		System.out.println("Race: " + get.Race());
+	}
 	
 	
 }
