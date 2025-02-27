7a8bc463c8e96dc58ac579893acc1c3eb65a0780
ginah903 <ginah903@gmail.com>
2025-02-27 00:00:55 -0600

    Created empty Main.java and initialized MiddleEarthCharacter.java
    Initialized MiddleEarthCharacter and created constructor for name,
    health, and power

---
+++ Assignment2/src/middleearthmadness/character/MiddleEarthCharacter.java
@@ -0,0 +1,26 @@
+package middleearthmadness.character;
+
+public abstract class MiddleEarthCharacter {
+	private String name;
+	private double health;
+	private double power;
+	
+	/**
+	 * Constructor that initializes name, health, and power
+	 * @param name Name of character
+	 * @param health Health level of character
+	 * @param power Power level of character
+	 */
+	public MiddleEarthCharacter(String name, double health, double power) {
+		super();
+		this.name = name;
+		this.health = health;
+		this.power = power;
+	}
+
+	
+
+	
+	
+	
+}
