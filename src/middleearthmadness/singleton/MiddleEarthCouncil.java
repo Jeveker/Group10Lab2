fb2d902bcaf76eb3920e51f7927b5b00654c9980
jackeveker <jackeveker@mwc-151083.dhcp.missouri.edu>
2025-02-27 20:55:09 -0600

    create getInstance method in MiddleEarthCouncil.java

--- src/middleearthmadness/singleton/MiddleEarthCouncil.java
+++ src/middleearthmadness/singleton/MiddleEarthCouncil.java
@@ -1,5 +1,20 @@
 package middleearthmadness.singleton;
 
 public class MiddleEarthCouncil {
+	
+	private static MiddleEarthCouncil instance;
+	
+	/**
+	 * Singleton method that returns a single instance
+	 * 
+	 * @return
+	 */
+	public static MiddleEarthCouncil getInstance() {
+		if(instance == null) {
+			instance = new MiddleEarthCouncil();
+		}
+		
+		return instance;
+	}
 
 }
