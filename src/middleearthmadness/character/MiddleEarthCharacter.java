1791baf149b210825068aac774830ca1ff1d53ab
ginah903 <ginah903@gmail.com>
2025-02-27 21:13:58 -0600

    Added getters and setters for MiddleEarthCharacter

--- src/middleearthmadness/character/MiddleEarthCharacter.java
+++ src/middleearthmadness/character/MiddleEarthCharacter.java
@@ -39,6 +39,32 @@ public abstract class MiddleEarthCharacter {
 		System.out.println("Power: " + name);
 		System.out.println("Race: " + getRace());
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getHealth() {
+		return health;
+	}
+
+	public void setHealth(double health) {
+		this.health = health;
+	}
+
+	public double getPower() {
+		return power;
+	}
+
+	public void setPower(double power) {
+		this.power = power;
+	}
+	
+	
 	
 	
 }
