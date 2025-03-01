981d5f12c520f1ca1a2c3036eb5fa4ba96ad4b75
ginah903 <ginah903@gmail.com>
2025-02-28 19:55:55 -0600

    Pushing Elf.java Finished attack method and getRace method

---
+++ src/middleearthmadness/character/Elf.java
@@ -0,0 +1,51 @@
+package middleearthmadness.character;
+
+public class Elf extends MiddleEarthCharacter {
+
+	public Elf(String name, double health, double power) {
+		super(name, health, power);
+	}
+
+	/**
+	 * Returns true if attack is successful and target's health is reduced.
+	 * Returns false if attack is unsuccessful and does no damage. 
+	 * @param target The target to attack.
+	 */
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		if(this.getRace().equals(target.getRace())) { // Elf vs. Elf
+			System.out.println("Ineffective attack! You cannot attack your own kin.");
+			return false;
+		}
+		
+		double damage = this.getPower(); 
+		
+		if (target.getRace().equals("Dwarf")) { // Elf vs. Dwarf
+			damage = 0;
+		} else if (target.getRace().equals("Orc")) { // Elf vs. Orc
+			damage *= 1.5;
+		}
+		
+		// Applies normal damage against Human and Wizard by default
+		
+		if (damage > 0) { // Reduce target's health
+			target.setHealth(target.getHealth() - damage);
+			System.out.println("Successful attack!");
+			return true;
+		} else {
+			System.out.println("Ineffective attack! Zero damage done.");
+			return false;
+		}
+	}
+
+	/**
+	 * Returns character's race.
+	 */
+	@Override
+	public String getRace() {
+		return "Elf";
+	}
+	
+	
+	
+}
