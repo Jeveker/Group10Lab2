e4d49fd36114fc221061c312aaaf860362945d26
ginah903 <ginah903@gmail.com>
2025-03-04 10:36:09 -0600

    Pushing getCharacter method and displayAllCharacter method in CharacterManager class

--- src/middleearthmadness/cms/CharacterManager.java
+++ src/middleearthmadness/cms/CharacterManager.java
@@ -1,5 +1,62 @@
 package middleearthmadness.cms;
 
-public class CharacterManager {
+import middleearthmadness.character.MiddleEarthCharacter;
 
+public class CharacterManager {
+	private MiddleEarthCharacter[] characters;
+	private int size;
+	
+	/**
+	 * Adds character to array, doubles array size if full.
+	 * @param c The character to add
+	 * @return Return true if successfully added, return false is character to add is invalid.
+	 */
+	boolean addCharacter(MiddleEarthCharacter c) {
+//		if (c == null) {
+//			System.out.println("Invalid character.");
+//			return false;
+//		}
+//		if (size == characters.length) {
+//			size *= 2;
+//		}
+//		
+//		return true;
+		
+	}
+	/**
+	 * Iterates through the characters in the array until one matches the given name and returns the matching MiddleEarthCharacter.
+	 * @param name The name of the character to return.
+	 * @return Returns null if name is invalid or if the character is not found, returns the MiddleEarthCharacter that matches the given name if found.
+	 */
+	MiddleEarthCharacter getCharacter(String name) {
+		if (name == null) {
+			System.out.println("Invalid character name.");
+			return null;
+		}
+		for (int i = 0; i < size; i++) {
+			if (characters[i].getName().equals(name)) {
+				System.out.println("Character found!");
+				return characters[i];
+			}
+		}
+		return null;
+	}
+	
+	boolean updateCharacter (MiddleEarthCharacter character, String name, int health, int power) {
+		
+	}
+	
+	boolean deleteCharacter(MiddleEarthCharacter character) {
+		
+	}
+	
+	/**
+	 * Displays information of all characters in the management system.
+	 */
+	void displayAllCharacters() {
+		for (int i = 0; i < size; i++) {
+			characters[i].displayInfo();
+		}
+	}
 }
+
