7e96571602549793adec8e9dcb37e8cee812b852
rai8d4 <122903297+rai8d4@users.noreply.github.com>
2025-03-05 05:38:58 -0600

    adding Main.java and Menu.java classes to MiddleEarthApp package

---
+++ src/middleearthapp/Main.java
@@ -0,0 +1,13 @@
+package middleearthapp;
+
+/**
+ * main class for Middle Earth Character Management System
+ * Initializes the application and invokes the menu
+ */
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/middleearthapp/Menu.java
@@ -0,0 +1,24 @@
+package middleearthapp;
+
+import java.util.Scanner;
+
+import middleearthmadness.cms.CharacterManager;
+
+/**
+ * Handles the menu-driven interface for managing Middle-earth characters.
+ */
+public class Menu {
+	private Scanner scanner;
+	private CharacterManager characterManager;
+
+	
+	/**
+	 * Constructor initializes scanner and imports CharacterManager
+	 */
+	public Menu() {
+		this.scanner = new Scanner(System.in);
+	}
+	
+	
+
+}
