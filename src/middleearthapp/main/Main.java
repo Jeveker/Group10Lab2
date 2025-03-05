c50d20b9ea72f9970f2de39868cdf52c90722451
jackeveker <jackeveker@wi000717fi.dhcp.missouri.edu>
2025-03-05 17:41:41 -0600

    Finished the main.java

--- src/middleearthapp/main/Main.java
+++ src/middleearthapp/main/Main.java
@@ -1,5 +1,7 @@
 package middleearthapp.main;
 
+import middleearthapp.menu.Menu;
+
 /**
  * main class for Middle Earth Character Management System
  * Initializes the application and invokes the menu
@@ -7,7 +9,11 @@ package middleearthapp.main;
 
 public class Main {
 	public static void main(String[] args) {
-
+		
+		Menu menu = new Menu();
+		//running the menu
+		menu.run();
+		
 	}
 
 }
